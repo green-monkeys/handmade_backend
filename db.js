b551daf7c8c530062b91fecde884cc293bcb8751
Maxwell Ray <mray02@calpoly.edu>
2019-04-04 19:41:50 -0700

    Addition of db.js

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
+export function testQuery() {
+  client.query(
+    "SELECT table_schema, table_name FROM information_schema.tables;",
+    (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+    }
+  );
+}
