7615a3eb1bb57ddc06532ab1f3e5fb794265812f
Maxwell Ray <mray02@calpoly.edu>
2019-04-07 19:58:02 -0700

    Addition of preliminary MWS functionality as well as first shot at .travis.yml

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const db = require("./db");
+const mws = require("./mws");
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/test_db", db.testQuery);
+const testRouter = express.Router();
+const dbRouter = express.Router();
+const mwsRouter = express.Router();
+
+testRouter.get("/", (req, res) => res.send("Hello World!"));
+dbRouter.get("/test_db", db.testQuery);
+mwsRouter.post("/list_mws_reports", mws.listMWSReports);
+mwsRouter.post("/hmac_encode", mws.hmacEncodeRequest);
+
+app.use("/", testRouter);
+app.use("/db", dbRouter);
+app.use("/mws", mwsRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
