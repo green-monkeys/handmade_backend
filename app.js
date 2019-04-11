e3b2bcd0b00600911b457b3ac8a7f4882f4db2d5
Maxwell Ray <mray02@calpoly.edu>
2019-04-11 15:49:00 -0700

    more improvements to dotenv

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import 'babel-polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-dotenv.config();
 import indexRouter from './routes/index';
 import dbRouter from './routes/db';
 import mwsRouter from './routes/mws';
--- controllers/DBController.js
+++ controllers/DBController.js
@@ -1,6 +1,4 @@
 import {Client} from 'pg';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
