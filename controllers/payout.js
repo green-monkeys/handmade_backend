5874a6f9cbd1ac7ae9f0162e39ce374a15aa3c1d
Maxwell Ray <mray02@calpoly.edu>
2019-04-30 13:37:42 -0700

    Payout ID Cleansing

--- controllers/payout.js
+++ controllers/payout.js
@@ -1,12 +1,13 @@
 import {query} from '../models/db';
 import * as payouts from '../models/payout';
 import {sendData, sendError} from "./responseHelper";
+import {idIsValid} from "./requestHelper";
 
 export async function getPayout(req, res) {
     const {id} = req.params;
 
-    if(isNaN(id)) {
-        sendError(res, 404, `Incorrect ID format. Fuck off Zane.`);
+    if(!idIsValid(id)) {
+        sendError(res, 404, `Incorrect ID format.`);
         return
     }
 
@@ -23,6 +24,16 @@ export async function getPayout(req, res) {
 export async function addPayout(req, res) {
     const {cgaId, artisanId, amount} = req.body;
 
+    if (!idIsValid(cgaId)) {
+        sendError(res, 400, `Invalid CGA ID`);
+        return
+    }
+    if (!idIsValid(artisanId)) {
+        sendError(res, 400, `Invalid Artisan ID`);
+        return;
+    }
+
+
     const payout = await payouts.addPayout(cgaId, artisanId, amount);
 
     if(!payout) {
@@ -34,6 +45,11 @@ export async function addPayout(req, res) {
 
 export async function removePayout(req, res) {
     const {id} = req.params;
+
+    if (!idIsValid(id)) {
+        sendError(res, 400, `Invalid ID Format`);
+        return
+    }
 
     const payout = await payouts.removePayout(id);
 
